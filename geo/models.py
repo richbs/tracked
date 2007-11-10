e0e61ac0c5ff215b032045ef05a9a3f366e23124
richbs <rickbs@gmail.com>
2007-11-10 00:55:12 +0000

    tweaking import script
    git-svn-id: https://svn.rbs.me.uk/tracked@46 97c8da76-f5bc-4bb7-965e-7cf1e57eaa11

--- geo/models.py
+++ geo/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-
 # Create your models here.
 class WayPoint(models.Model):
     """(WayPoint description)"""
+    latitude = models.FloatField(max_digits=12, decimal_places=9)
+    longitude = models.FloatField(max_digits=12, decimal_places=9)
+    altitude = models.FloatField(max_digits=10, decimal_places=6)
+    time = models.DateTimeField()
     
     class Admin:
         pass
@@ -30,33 +33,38 @@ class GpxFile(models.Model):
         """
         Convert XML points to WayPoint modes
         """
-        
-        
-class Trip(models.Model):
-    """(Trip description)"""
-    name        = models.CharField( maxlength=100)
-    description = models.CharField(blank=True, maxlength=255)
-    start_time  = models.DateTimeField()
-    end_time    = models.DateTimeField()
-    length = models.FloatField(max_digits=10, decimal_places=5)
-
-    class Admin:
         pass
-
-    def __str__(self):
-        return "Trip"
 
 class Track(models.Model):
     """(Track description)"""
+    def __str__(self):
+        return "Track"
+
     name        = models.CharField( maxlength=100)
     description = models.CharField(blank=True, maxlength=255)
     start_time  = models.DateTimeField()
     end_time    = models.DateTimeField()
     length = models.FloatField(max_digits=10, decimal_places=5)
     waypoints   = models.ManyToManyField(WayPoint)
+
     class Admin:
         pass
-        
-    def __str__(self):
-        return "Track"
 
+class Trip(models.Model):
+    def __str__(self):
+        return "Trip"
+        
+    """(Trip description)"""
+    name        = models.CharField( maxlength=100)
+    description = models.CharField(blank=True, maxlength=255)
+    start_time  = models.DateTimeField()
+    end_time    = models.DateTimeField()
+    length = models.FloatField(max_digits=10, decimal_places=5)
+    tracks = models.ManyToManyField(Track)
+    
+    class Admin:
+        pass
+
+
+
+    
--- pdomtest.py
+++ pdomtest.py
@@ -1,6 +1,43 @@
 from xml.dom import pulldom
 print pulldom.__name__
+
+import urllib2, re, codecs, os, sys
+import time, datetime
+
+def setup_environ(settings_mod):
+    """
+    Configure the runtime environment. This can also be used by external
+    scripts wanting to set up a similar environment to manage.py.
+    """
+    # Add this project to sys.path so that it's importable in the conventional
+    # way. For example, if this file (manage.py) lives in a directory
+    # "myproject", this code would add "/path/to/myproject" to sys.path.
+    project_directory = os.path.dirname(settings_mod.__file__)
+    project_name = os.path.basename(project_directory)
+    sys.path.append(os.path.join(project_directory, '..'))
+    project_module = __import__(project_name, {}, {}, [''])
+    sys.path.pop()
+
+    # Set DJANGO_SETTINGS_MODULE appropriately.
+    os.environ['DJANGO_SETTINGS_MODULE'] = '%s.%s' % (project_name, settings_mod.__name__)
+    return project_directory
+
+import settings
+setup_environ(settings)
+
+
+from tracked.geo.models import Trip, Track, WayPoint, GpxFile
+
+gpx = GpxFile()
+gpx.name = 'My Yorkshire Trip'
+gpx.filename = 'yorkshire.gpx'
+gpx.save()
+
+print gpx
+
 gpxlog = pulldom.parse('yorkshire.gpx')
+
+
 for event,node in gpxlog:
     # Only construct a dom from the track points
     if event == 'START_ELEMENT' and node.nodeName == 'trkpt':
@@ -12,5 +49,18 @@ for event,node in gpxlog:
             elenode = node.getElementsByTagName('ele')
             timestring = timenode[0].firstChild.nodeValue
             elestring = elenode[0].firstChild.nodeValue
+            lat = node.getAttribute('lat')
+            lon = node.getAttribute('lon')
+
+            w = WayPoint()
+            w.latitude = lat
+            w.longitude = lon
+            w.altitude = elestring
+            w.time =  datetime.datetime.strptime(timestring, '%Y-%m-%dT%H:%M:%SZ')
+            w.save()
+            gpx.waypoints.add(w)
+            gpx.save()
+            
+            
         else:
             pass # This is not a active log way point
