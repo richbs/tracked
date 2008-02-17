361988762e3a30d8983e426d44190fa85ae78b55
richbs <rickbs@gmail.com>
2008-02-17 00:42:00 +0000

    live settings
    git-svn-id: https://svn.rbs.me.uk/tracked@86 97c8da76-f5bc-4bb7-965e-7cf1e57eaa11

--- settings_live.py
+++ settings_live.py
@@ -1,3 +1,11 @@
-from geo.settings import *
+from tracked.settings import *
 DEBUG = False
 DATABASE_HOST = 'localhost'
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/home/offspinner/webapps/mytracks/tracked/geo/templates',
+    
+)
+MEDIA_ROOT = '/home/offspinner/webapps/mytracks/tracked'
