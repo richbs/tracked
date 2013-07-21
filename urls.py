4ca24640e4c21fdcc40a2a525a9610684256db11
Richard Barrett-Small <rickbs@gmail.com>
2013-07-21 18:22:13 +0100

    Sorting out urls.py as part of tidy up #2.

--- urls.py
+++ urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls.defaults import *
-from tracked import settings
+from django.conf.urls.defaults import include, patterns
 from django.contrib import admin
 from django.conf import settings
 
@@ -11,14 +10,14 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^dates/(?P<date_from>\d{8})-(?P<date_to>\d{8})/?', 'tracked.geo.views.dates'),
     # (r'^upload/?', 'tracked.geo.views.upload'),
-    (r'^track/(?P<track_id>[0-9]+)/?$','tracked.geo.views.show_track'),
-    (r'^track/(?P<track_id>[0-9]+).kml/?$','tracked.geo.views.kml_track'),    
-    (r'^getphotos/(?P<track_id>[0-9]+)/?$','tracked.geo.views.get_photos'),    
-    (r'^os-track/(?P<track_id>[0-9]+)/?$','tracked.geo.views.show_os_track'),
-    (r'^os-route/?$','tracked.geo.views.os_route'),    
-    (r'^btracks/?$','tracked.geo.views.between_tracks'),
-    (r'^geotag/?$','tracked.geo.views.geo_tag'),
-    (r'^fauth/?$','tracked.geo.views.callback'),    
+    (r'^track/(?P<track_id>[0-9]+)/?$', 'tracked.geo.views.show_track'),
+    (r'^track/(?P<track_id>[0-9]+).kml/?$', 'tracked.geo.views.kml_track'),
+    (r'^getphotos/(?P<track_id>[0-9]+)/?$', 'tracked.geo.views.get_photos'),
+    (r'^os-track/(?P<track_id>[0-9]+)/?$', 'tracked.geo.views.show_os_track'),
+    (r'^os-route/?$', 'tracked.geo.views.os_route'),
+    (r'^btracks/?$', 'tracked.geo.views.between_tracks'),
+    (r'^geotag/?$', 'tracked.geo.views.geo_tag'),
+    (r'^fauth/?$', 'tracked.geo.views.callback'),
     (r'^/?$', 'tracked.geo.views.home_page'),
     (r'index', 'tracked.geo.views.home_page'),
 )
